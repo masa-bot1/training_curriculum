1e87a7fb311d0a69b5ff493b747ef901fda99af1
masa-bot1 <bdyht515@yahoo.co.jp>
2020-10-27 17:44:54 +0900

    Revert "Merge pull request #9 from masa-bot1/second_training"
    This reverts commit 5796f5f20c83c040bb75124bb72e81bb87b6b70a, reversing
    changes made to e76113fbae1cfa4e6fdf3a76bb30358c7611660b.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -15,7 +15,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:plan).permit(:date, :plan)
+    params.require(:calendars).permit(:date, :plan)
   end
 
   def get_week
@@ -34,14 +34,7 @@ class CalendarsController < ApplicationController
       plan = plans.map do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-
-      wday_num = Date.today.wday + x#Date.today.wdayを利用して添字となる数値を得る
-      #もしもwday_numが7以上であれば、7を引く
-      if wday_num >= 7 #条件式を記述
-        wday_num = wday_num - 7
-      end
-      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: week_days[wday_num] }
-
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
       @week_days.push(days)
     end
 
